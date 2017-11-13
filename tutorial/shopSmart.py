11fcc896fa9a113bbf914b15b3f04fe748444e8a
isaacrr <DS\u104031@54005p43.aulesca.upf.edu>
2017-11-13 16:56:43 +0100

    Project 0: Unix/Python/Autograder Tutorial, Question 3: shopSmart function

--- tutorial/shopSmart.py
+++ tutorial/shopSmart.py
@@ -29,16 +29,32 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
+    
+    price = float("Inf")
+    botiga = 0
+    
+    for index,shop in enumerate(fruitShops):
+		var = shop.getPriceOfOrder(orderList) 
+		if var <= price:
+			price = var
+			botiga = index
+    
+    return fruitShops[botiga]
 
 if __name__ == '__main__':
   "This code runs when you invoke the script from the command line"
   orders = [('apples',1.0), ('oranges',3.0)]
+  
   dir1 = {'apples': 2.0, 'oranges':1.0}
   shop1 =  shop.FruitShop('shop1',dir1)
+  
   dir2 = {'apples': 1.0, 'oranges': 5.0}
   shop2 = shop.FruitShop('shop2',dir2)
+  
   shops = [shop1, shop2]
+  
   print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+  
   orders = [('apples',3.0)]
+  
   print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
