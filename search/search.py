c0a97354d94c0e34a4993cc471987d520707a01d
Isaac Rossell Ripoll <isaac1842@gmail.com>
2017-11-22 21:31:26 +0100

    BFS començat

--- search/search.py
+++ search/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -71,7 +71,7 @@ def tinyMazeSearch(problem):
     s = Directions.SOUTH
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
-    
+
 def DFS(states, problem, closeList):
 	for s in states[::-1]:
 		if s[0] in closeList or s is None:
@@ -92,7 +92,7 @@ def depthFirstSearch(problem):
 
 	To get started, you might want to try some of these simple commands to
 	understand the search problem that is being passed in:
-	
+
 	print "Start:", problem.getStartState()
 	print "Is the start a goal?", problem.isGoalState(problem.getStartState())
 	print "Start's successors:", problem.getSuccessors(problem.getStartState())
@@ -101,7 +101,7 @@ def depthFirstSearch(problem):
 	'''
 	Quan fiques, es posa a sobre.
 	Quan consultes, treu el primer.
-	
+
 	Exemple: A|B|C
 	Mentre stack No Buit:
 	Pop A
@@ -112,27 +112,25 @@ def depthFirstSearch(problem):
 	Push de tots els fill d'A -> D|E|F B|C
 	Posar A Vist
 	'''
-	
+
 	'''
-	stack = utils.Stack()							 
+	stack = utils.Stack()
 	start = (problem.getStartState(), None, None)	# ((x, y), direccio, cost)
 	stack.push(start)
-	
+
 	while not stack.isEmpy():
-		
-	
+
+
 	util.raiseNotDefined()
 	'''
-	
+
 	"*** YOUR CODE HERE ***"
-	
+
 	closeList = set()				# Un Set normal
 	start = problem.getStartState()
 	closeList.add(start)
-		
-	return DFS(problem.getSuccessors(start), problem, closeList)
 
-def DFS(states, problem, closeList):
+	return DFS(problem.getSuccessors(start), problem, closeList)
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
@@ -140,7 +138,7 @@ def breadthFirstSearch(problem):
     '''
 	Quan fiques, es posa a sota.
 	Quan consultes, treu el primer.
-	
+
 	Exemple: A|B|C
 	Mentre queu No Buit:
 	Pop A
@@ -151,7 +149,31 @@ def breadthFirstSearch(problem):
 	Push de tots els fill d'A -> B|C D|E|F
 	Posar A Vist
 	'''
-    util.raiseNotDefined()
+    start = (problem.getStartState(), None, None)
+
+    closeList = set()
+    output =  []
+    queue = util.Queue()
+
+    queue.push(start)
+
+    while not queue.isEmpty():
+        state = queue.pop()
+        if state in closeList or state[0] is None:
+            continue
+        if problem.isGoalState(state[0]):
+            while state[1]:
+                output.append(state[1])
+                for parent in closeList:
+                    if parent[0][0] == state[2][0] and parent[0][1] == state[2][1]:
+                        state = parent
+                        break
+            break
+        for child in problem.getSuccessors(state[0]):
+            queue.push((child[0], child[1], state[0]))
+        closeList.add(state)
+
+    return output[::-1]
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
@@ -159,9 +181,9 @@ def uniformCostSearch(problem):
     '''
     start = problem.getStartState()
 	closeList.add(start)
-	
+
 	queu = utils.Queue
-	
+
 	for s in queu.pop:
 		if s[0] in closeList or s is None:
 			continue
